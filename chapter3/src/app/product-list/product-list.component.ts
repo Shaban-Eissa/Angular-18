cb0331ec505a855898cb55b30d3be1e0c5913d42
Shaban-Eissa <shabaneissa767@gmail.com>
2024-08-12 10:09:19 +0300

    ✨ New Feature:  Adding product list & using @for in template to iterate over data

--- chapter3/src/app/product-list/product-list.component.ts
+++ chapter3/src/app/product-list/product-list.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Product } from "../product";
 
 @Component({
-  selector: 'app-product-list',
+  selector: "app-product-list",
   standalone: true,
   imports: [],
-  templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  templateUrl: "./product-list.component.html",
+  styleUrl: "./product-list.component.css",
 })
 export class ProductListComponent {
-
+  selectedProduct: Product | undefined;
+  products: Product[] = [
+    { id: "1", title: "Keyboard" },
+    { id: "2", title: "Microphone" },
+    { id: "3", title: "Mouse" },
+  ];
 }
