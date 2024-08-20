3ee701d115b2473a398a81316637e52cb2892480
Shaban-Eissa <shabaneissa767@gmail.com>
2024-08-20 14:52:36 +0300

    🎉 Initial Commit: Adding basic files for template driven forms

---
+++ chapter10/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductCreateComponent } from './product-create/product-create.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { authGuard } from './auth.guard';
+import { checkoutGuard } from './checkout.guard';
+export const routes: Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'products/new', component: ProductCreateComponent },
+  {
+    path: 'cart',
+    component: CartComponent,
+    canActivate: [authGuard],
+    canDeactivate: [checkoutGuard],
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./user.routes').then((m) => m.default),
+    canMatch: [authGuard],
+  },
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: '**', redirectTo: 'products' },
+];
